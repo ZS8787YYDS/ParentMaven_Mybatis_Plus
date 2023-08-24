377ab0de8bb77ef4eb05f2b3c509c900ab322124
zishengyyds <zisheng8787@gitee.com>
2023-08-24 16:27:30 +0800

    add module

---
+++ Mybatis_plus_02_qucikStart/src/main/java/com/zisheng8787/MybatisPlus02QucikStartApplication.java
@@ -0,0 +1,13 @@
+package com.zisheng8787;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlus02QucikStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlus02QucikStartApplication.class, args);
+    }
+
+}
